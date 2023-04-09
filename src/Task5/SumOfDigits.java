8a66603dbbe74eaef1aef30219683af089b17283
Vitalii <vitaliy5saie@gmail.com>
2023-04-09 18:48:57 +0200

    added home-task5 - recursion

---
+++ src/Task5/SumOfDigits.java
@@ -0,0 +1,20 @@
+package Task5;
+
+/*
+Given a natural number N. Calculate the sum of its digits.
+You cannot use strings, lists, arrays and loops.
+ */
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int answer = calculateSumOfDigits(1234567);
+        System.out.println(answer);
+    }
+
+    private static int calculateSumOfDigits(int i) {
+
+        if (i / 10 < 1) {
+            return i;
+        }
+        return calculateSumOfDigits(i / 10) + (i % 10);
+    }
+}
