7dc202a560eee6cc22e8321456ddfad25abea3a4
Vitalii <vitaliy5saie@gmail.com>
2023-05-29 16:49:19 +0200

    added homework - greedy algorithms and listMode

---
+++ src/Task11/FurnitureFactory/Box.java
@@ -0,0 +1,37 @@
+package Task11.FurnitureFactory;
+/*
+Вы работаете в фирме по производству мебели и поставляете мебель по всей стране.
+Коробки с мебелью размещаются в грузовике. Все коробки имеют разный размер, и вы стараетесь наиболее
+эффективно использовать доступное пространство. Как выбрать коробки для того, чтобы загрузка имела максимальную
+эффективность? Предложите жадную стратегию. Будет ли полученное решение оптимальным?
+ */
+public class Box {
+    private int weightKilogram;
+    private int priceOfItemInBox;
+
+    public Box(int weightKilogram, int priceOfItemInBox) {
+        this.weightKilogram = weightKilogram;
+        this.priceOfItemInBox = priceOfItemInBox;
+    }
+
+    public int getWeightKilogram() {
+        return weightKilogram;
+    }
+
+    public void setWeightKilogram(int weightKilogram) {
+        this.weightKilogram = weightKilogram;
+    }
+
+    public int getPriceOfItemInBox() {
+        return priceOfItemInBox;
+    }
+
+    public void setPriceOfItemInBox(int priceOfItemInBox) {
+        this.priceOfItemInBox = priceOfItemInBox;
+    }
+
+    @Override
+    public String toString() {
+        return "Box(" + weightKilogram + " kilogram, " + priceOfItemInBox + " USD"+')';
+    }
+}
---
+++ src/Task11/LinkedList/ListNode.java
@@ -0,0 +1,25 @@
+package Task11.LinkedList;
+/*
+Given the head of a linked list, remove the n-th node from the end of the list and return its head.
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+}
