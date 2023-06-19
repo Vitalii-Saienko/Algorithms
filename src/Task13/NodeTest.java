166353ad5270a73a10416950c83392cf993a2acd
Vitalii <vitaliy5saie@gmail.com>
2023-06-19 17:48:27 +0200

    added deleteNode method

--- src/Task13/NodeTest.java
+++ src/Task13/NodeTest.java
@@ -4,11 +4,11 @@ package Task13;
 public class NodeTest {
     public static void main(String[] args) {
         /*
-                          100                           | 1
-                   25               150                 | 2
-               15     75       130       180            | 3
-                        76                  181         | 4
-                                              182       | 5
+                          100                            | 1
+                   25                150                 | 2
+               15     75       130         180           | 3
+                        76              165     181      | 4
+                                     164  166       182  | 5
          */
 
         Node root = new Node(100);
@@ -20,6 +20,9 @@ public class NodeTest {
         root.rightChild = new Node(150);
         root.rightChild.leftChild = new Node(130);
         root.rightChild.rightChild = new Node(180);
+        root.rightChild.rightChild.leftChild = new Node(165);
+        root.rightChild.rightChild.leftChild.leftChild = new Node(164);
+        root.rightChild.rightChild.leftChild.rightChild = new Node(166);
         root.rightChild.rightChild.rightChild = new Node(181);
         root.rightChild.rightChild.rightChild.rightChild = new Node(182);
 
@@ -27,6 +30,10 @@ public class NodeTest {
         Node.iterativeTreeTraversal(root);
 
         System.out.println("Task2 - find height: " + Node.findTreeHeight(root));
+
+        System.out.println("Task3 - finish delete method, lets delete node with value=180: ");
+        root.delete(root.rightChild.rightChild, root);
+        Node.iterativeTreeTraversal(root);
 
     }
 }
